e5ae6a33b79ed047a9bbaa3a7746ed7c142b36da
Mete Yildiz <mete.yildiz@smail.th-koeln.de>
2020-05-08 18:31:43 +0200

    2te seite geaddet plus Button next seite funktioniert Einrichten der 2ten noch nciht ganz hingekriegt

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val testModusBtn = findViewById<Button>(R.id.Testmodusbtn)
+        testModusBtn.setOnClickListener{
+            val intent = Intent(this, WissenLevelAcitivity::class.java)
+            startActivity(intent)
+        }
+
+        val lernModusBtn = findViewById<Button>(R.id.Lernmodusbtn)
+        lernModusBtn.setOnClickListener{
+            val intent = Intent(this, WissenLevelAcitivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/quizapp/WissenLevelAcitivity.kt
@@ -0,0 +1,12 @@
+package com.example.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WissenLevelAcitivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wissen_level_acitivity)
+    }
+}
