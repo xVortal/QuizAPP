8f4b611a54f8794c388322f05370255a970b4970
Mete Yildiz <mete.yildiz@smail.th-koeln.de>
2020-05-12 13:03:34 +0200

    versucht die datenbank zu verknuepfen

--- app/src/main/java/com/example/quizapp/DataBaseHelper.kt
+++ app/src/main/java/com/example/quizapp/DataBaseHelper.kt
@@ -1,13 +1,17 @@
 package com.example.quizapp
 
+import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 
 import android.database.sqlite.SQLiteOpenHelper
 
-class DataBaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, 1) {
+class DataBaseHelper(context: Context):SQLiteOpenHelper(context, DATABASE_NAME, null, 1) {
     companion object{
         val DATABASE_NAME = "quiz.db"
         val TABLE_NAME = "question_table"
+        val QUESTION_ID = "ID"
         val QUESTION = "QUESTION"
         val ANSWERA = "ANSWERA"
         val ANSWERB = "ANSWERB"
@@ -17,7 +21,52 @@ class DataBaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME
         val DIFFICULTY = "DIFFICULTY"
     }
 
-    override fun onCreate(SQLiteDatabase){
-        db.execSQL("CREATE TABLE $TABLE_NAME")
+    override fun onCreate(db: SQLiteDatabase){
+        db.execSQL("CREATE TABLE $TABLE_NAME(ID INTEGER PRIMARY KEY "+"AUTOINCREMENT,QUESTION TEXT, ANSWERA TEXT," +
+                "ANSWERB TEXT,ANSWERC TEXT,ANSWERD TEXT,CORRECTANSWER TEXT,DIFFICULTY INTEGER,)")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+        db.execSQL("DROP TABLE IF EXISTS"+ TABLE_NAME)
+    }
+
+    fun insertDATA(question:String, answera:String, answerb:String, answerc:String, answerd:String, correctanswer:String, difficulty:Int){
+        val db = this.writableDatabase
+        val contentvalues = ContentValues()
+        contentvalues.put(QUESTION,question)
+        contentvalues.put(ANSWERA,answera)
+        contentvalues.put(ANSWERB,answerb)
+        contentvalues.put(ANSWERC,answerc)
+        contentvalues.put(ANSWERD,answerd)
+        contentvalues.put(CORRECTANSWER,correctanswer)
+        contentvalues.put(DIFFICULTY,difficulty)
+        db.insert(TABLE_NAME, null, contentvalues)
+    }
+
+    fun updateDATA(id:String, question:String, answera:String, answerb:String, answerc:String, answerd:String, correctanswer:String, difficulty:Int){
+        val db = this.writableDatabase
+        val contentvalues = ContentValues()
+        contentvalues.put(QUESTION_ID,id)
+        contentvalues.put(QUESTION,question)
+        contentvalues.put(ANSWERA,answera)
+        contentvalues.put(ANSWERB,answerb)
+        contentvalues.put(ANSWERC,answerc)
+        contentvalues.put(ANSWERD,answerd)
+        contentvalues.put(CORRECTANSWER,correctanswer)
+        contentvalues.put(DIFFICULTY,difficulty)
+        db.update(TABLE_NAME, contentvalues, "QUESTION_ID=?", arrayOf(id))
+    }
+
+    fun deleteDATA(id:String):Int{
+        val db=this.writableDatabase
+        return db.delete(TABLE_NAME, "ID=?", arrayOf(id))
+    }
+
+    val showData:Cursor
+    get(){
+        val db=this.writableDatabase
+        val result = db.rawQuery("SELECT * FROM " + TABLE_NAME, null)
+        return result
     }
 }
--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
+    internal var dbhelper = DataBaseHelper(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,5 +27,41 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, WissenLevelAcitivity::class.java)
             startActivity(intent)
         }
+    }
+
+    fun  showDialog(title:String, Message:String){
+        val builder = AlertDialog.Builder(this)
+        builder.setCancelable(true)
+        builder.setTitle(title)
+        builder.setMessage(Message)
+        builder.show()
+    }
+
+    fun clear(){
+        question_id.setText("")
+        question.setText("")
+        answera.setText("")
+        answerb.setText("")
+        answerc.setText("")
+        answerd.setText("")
+        correctanswer.setText("")
+        difficulty.setText("")
+    }
+
+    fun insert(){
+        Testmodusbtn.setOnClickListener(
+            try {
+                dbhelper.insertDATA(question.text.toString(), answera.text.toString(), answerb.text.toString(),
+                    answerc.text.toString(), answerd.text.toString(), correctanswer.text.toString(), difficulty.text.toString())
+                clear()
+            }
+            catch (e:Exception){
+                e.printStackTrace()
+                Toast.makeText(this, e.message.toString(), Toast.LENGTH_LONG.).show()
+            }
+        )
+    }
+    fun update(){
+
     }
 }
